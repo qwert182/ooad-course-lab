dfb3cac9113e2497e53acd31667d7686d04ec6e5
Грошева Людмила <qwert183@users.noreply.github.com>
2016-11-28 21:26:07 +0300

    fixed Exception by adding ctor and =

--- src/commands_system/utils/Exception.h
+++ src/commands_system/utils/Exception.h
@@ -2,9 +2,13 @@
 
 class Exception {
 	char *s;
-protected:
-	Exception(const char *what);
+	void init(const char *what);
 public:
+	Exception(const char *what);
+	Exception(const Exception &);
+	Exception & operator =(const Exception &);
+	Exception(Exception &&);
+	Exception & operator =(Exception &&);
 	virtual const char * what() const;
 	virtual ~Exception();
 };
