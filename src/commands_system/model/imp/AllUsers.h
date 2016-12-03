d68704511f41488731ede46c0193bbe62e4bd4c5
DenWolf <denwolk@mail.ru>
2016-12-03 12:36:13 +0300

    Fixed imp/AllUsers.h

--- src/commands_system/model/imp/AllUsers.h
+++ src/commands_system/model/imp/AllUsers.h
@@ -8,8 +8,8 @@ public:
 	virtual std::vector<class IUser *> getUsers() const;
 	virtual void add(const class IUser &user);
 	virtual void remove(const class IUser &user);
-	virtual class IUser * logIn(const class IUser &user);
-	virtual void signUp();
+	virtual class IUser * logIn(const std::string &login, const std::string &password);
+	virtual class IUser * signUp(const class RegForm &regForm);
 
 	AllUsers();
 	virtual ~AllUsers() {}
