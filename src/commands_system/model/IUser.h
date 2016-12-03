9cc7c0a02603ac4071d0696190ce1dd07222e3c3
DenWolf <denwolk@mail.ru>
2016-12-03 12:22:48 +0300

    Fixed IUser.h: & on *

--- src/commands_system/model/IUser.h
+++ src/commands_system/model/IUser.h
@@ -14,11 +14,11 @@ public:
 	virtual std::string getPassword() const = 0;
 	virtual void setPassword(const std::string password) const = 0;
 
-	virtual class IUserType & getType() const = 0;
+	virtual class IUserType * getType() const = 0;
 	virtual void setType(const class IUserType &userType) const = 0;
 
-	virtual class IMail & getMail() const = 0;
-	virtual class IAllProjects & getProjects() const = 0;
+	virtual class IMail * getMail() const = 0;
+	virtual class IAllProjects * getProjects() const = 0;
 
 	virtual void logOut() = 0;
 };
