3bbc3417e8e66247ff6a0e957cb0ae8427166b66
DenWolf <denwolk@mail.ru>
2016-12-03 15:43:10 +0300

    Add cpp-files

---
+++ src/commands_system/model/imp/UserType.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "UserType.h"
+
+UserType::UserType(int id) {
+
+}
+
+UserType::UserType(const std::string &name, int type) {
+
+}
+
+std::string UserType::getName() const {
+	std::string result;
+	return result;
+}
+
+void UserType::setName(const std::string &name) {
+
+}
+
+bool UserType::isManager() const {
+	bool result;
+	return result;
+}
+
+bool UserType::isAdmin() const {
+	bool result;
+	return result;
+}
+
