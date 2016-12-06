9399b008a81bb2ca861e08733e621f936fb9698e
DenWolf <denwolk@mail.ru>
2016-12-06 13:34:37 +0300

    Add Message: get-methods and tests

---
+++ src/commands_system/model/imp/test_Message.cpp
@@ -0,0 +1,60 @@
+#ifdef COMPILE_WITH_TESTS
+#include "test_WithFullDB.h"
+
+#include "User.h"
+#include "Message.h"
+
+/*
+TEST_from(GetSenderTest, WithFullDB) {
+	void test() {
+		
+		IMessage *message = new Message(1);
+		IUser *result = message->getSender();
+
+		assertEquals("volkov", result->getLogin());
+				
+		delete message;
+		delete result;
+	}
+} END_TEST;
+
+TEST_from(GetReceiverTest, WithFullDB) {
+	void test() {
+		
+		IMessage *message = new Message(1);
+		IUser *result = message->getReceiver();
+
+		assertEquals("solovev", result->getLogin());
+				
+		delete message;
+		delete result;
+	}
+} END_TEST;
+
+TEST_from(GetSubjectTest, WithFullDB) {
+	void test() {
+		
+		IMessage *message = new Message(1);
+		std::string result = message->getSubject();
+
+		assertEquals("time", result);
+				
+		delete message;
+	}
+} END_TEST;
+
+TEST_from(GetTextTest, WithFullDB) {
+	void test() {
+		
+		IMessage *message = new Message(1);
+		std::string result = message->getText();
+
+		assertEquals("you're late", result);
+				
+		delete message;
+	}
+} END_TEST;
+*/
+
+#endif
+
