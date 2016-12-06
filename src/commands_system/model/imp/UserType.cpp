bc5afd3e9010c2269cb3f365b0117ec19a180502
DenWolf <denwolk@mail.ru>
2016-12-06 14:52:54 +0300

    Add UserType: methods and tests

--- src/commands_system/model/imp/UserType.cpp
+++ src/commands_system/model/imp/UserType.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "UserType.h"
+#include "dataBase.h"
 
 UserType::UserType(int id) {
 	this->id = id;
@@ -12,6 +13,13 @@ UserType::UserType(const std::string &name, int type) {
 
 std::string UserType::getName() const {
 	std::string result;
+
+	ptrTable t = dataBase->perform(
+		SELECT_ONLY("name").from("usertypes").where("id", this->id)
+	);
+
+	result = t->get(0, 0);
+
 	return result;
 }
 
@@ -20,12 +28,32 @@ void UserType::setName(const std::string &name) {
 }
 
 bool UserType::isManager() const {
-	bool result;
+	bool result = false;
+
+	ptrTable t = dataBase->perform(
+		SELECT_ONLY("type").from("usertypes").where("id", this->id)
+	);
+
+	int type = t->get(0, 0);
+	if(type == 1) {
+		result = true;
+	}	
+
 	return result;
 }
 
 bool UserType::isAdmin() const {
-	bool result;
+	bool result = false;
+
+	ptrTable t = dataBase->perform(
+		SELECT_ONLY("type").from("usertypes").where("id", this->id)
+	);
+
+	int type = t->get(0, 0);
+	if(type == 0) {
+		result = true;
+	}	
+
 	return result;
 }
 
