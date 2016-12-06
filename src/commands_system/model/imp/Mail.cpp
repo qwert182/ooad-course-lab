b6544d106787fc69b4843962a67593a008bd7417
DenWolf <denwolk@mail.ru>
2016-12-06 17:30:47 +0300

    Add Mail: methods and tests

--- src/commands_system/model/imp/Mail.cpp
+++ src/commands_system/model/imp/Mail.cpp
@@ -36,6 +36,23 @@ std::vector<class IMessage *> Mail::getInBox() const {
 
 std::vector<class IMessage *> Mail::getOutBox() const {
 	std::vector<class IMessage *> result;
+
+	ptrTable t = dataBase->perform(
+		SELECT_ONLY("id").from("messages").where("receiver_id", this->user->getId())
+	);
+
+	int numOfMessages = t->getRowCount();
+	int id;
+	IMessage *message;
+
+	result.reserve(numOfMessages);
+
+	for(int i = 0; i < numOfMessages; i++) {
+		id = t->get(i, 0);
+		message = new Message(id);
+		result.push_back(message);
+	}
+
 	return result;
 }
 
--- src/commands_system/model/imp/test_Mail.cpp
+++ src/commands_system/model/imp/test_Mail.cpp
@@ -12,7 +12,9 @@ TEST_from(GetInBoxTest_IsNotEmpty, WithFullDB) {
 		IMail *mailInBox = new Mail(user);
 		std::vector<class IMessage *> result = mailInBox->getInBox();
 		
-		assertEquals("volkov", result[0]->getSender()->getLogin());
+		IUser *u = result[0]->getSender();
+
+		assertEquals("volkov", u->getLogin());
 
 		int n = result.size();
 		for(int i = 0; i < n; i++) {
@@ -21,11 +23,56 @@ TEST_from(GetInBoxTest_IsNotEmpty, WithFullDB) {
 		
 		delete mailInBox;
 		delete user;
+		delete u;
 	}
 } TEST_END;
 
+TEST_from(GetInBoxTest_IsEmpty, WithFullDB) {
+	void test() {
+		User *user = new User(1);
+		IMail *mailInBox = new Mail(user);
+		std::vector<class IMessage *> result = mailInBox->getInBox();		
+		
+		assertEquals(true, result.empty());
+		
+		delete mailInBox;
+		delete user;
+	}
+} TEST_END;
 
+TEST_from(GetOutBoxTest_IsNotEmpty, WithFullDB) {
+	void test() {
+		User *user = new User(1);
+		IMail *mailOutBox = new Mail(user);
+		std::vector<class IMessage *> result = mailOutBox->getOutBox();
+		
+		IUser *u = result[0]->getReceiver();
 
+		assertEquals("solovev", u->getLogin());
+
+		int n = result.size();
+		for(int i = 0; i < n; i++) {
+			delete result[i];
+		}		
+		
+		delete mailOutBox;
+		delete user;
+		delete u;
+	}
+} TEST_END;
+
+TEST_from(GetOutBoxTest_IsEmpty, WithFullDB) {
+	void test() {
+		User *user = new User(2);
+		IMail *mailOutBox = new Mail(user);
+		std::vector<class IMessage *> result = mailOutBox->getOutBox();
+
+		assertEquals(true, result.empty());
+		
+		delete mailOutBox;
+		delete user;
+	}
+} TEST_END;
 
 #endif
 
